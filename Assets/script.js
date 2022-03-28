d3d5133bcf2d98f5104590bea8904f705a63f044
Rembrandt <95456210+chrisnunez@users.noreply.github.com>
2022-03-27 18:48:30 -0700

    updated javascript

--- Assets/script.js
+++ Assets/script.js
@@ -6,41 +6,40 @@ var startbtn = document.querySelector('.startbtn') ;
 var highScore = document.getElementById('highscore');
 var mainContainer = document.querySelector('.mainContainer');
 var containerBtn = document.querySelector('.containerbtn');
-var choices = document.querySelectorAll('.choice');
-var choiceContainer = document.querySelector('.Choices-container')
+// var choices = document.querySelectorAll('.choice');
+// var choiceContainer = document.querySelector('.Choices-container')
 
 // Questions that will display after button is clicked
 var questions = [
     {
+        // Question 0
     Question: 'This is question 1',
-    choiceA: 'this is choice 1',
-    choiceB: 'choice 2',
-    choiceC: 'choice 3',
-    choiceD: 'choice 4',
-    correct: 'choice 4'
+    choices: ['choice1', 'choice2', 'choice3', 'choice4'],
+    correctAnswer: 'choice3'
 },
 { 
+    // Question 1
     Question: 'This is question 2',
-    choiceA: 'this is choice 1/2',
-    choiceB: 'choice 2',
-    choiceC: 'choice 3',
-    choiceD: 'choice 4',
-    correct: 'choice 2'
+    choices: ['choice1', 'choice2', 'choice3', 'choice4'],
+    correctAnswer: 'choice1'
 }
 ]
 
+function getQuestions(questionIndex) {
+
+}
 
 
 
 // Gets DOM and creates variables for questions
 var questionContainer = document.querySelector('.Questions-container');
 var questionEl = document.getElementById('questionEl')
-var choice1 = document.getElementById('choice1')
+var choice1 = document.querySelector('#choice1')
 var choice2 = document.getElementById('choice2')
 var choice3 = document.getElementById('choice3') 
 var choice4 = document.getElementById('choice4')
 var questionLength = questions.length;
-var questionStart = 0;
+var questionIndex = 0;
 var score = 0;
 
 
@@ -49,10 +48,36 @@ var score = 0;
 // Function that starts quiz and first question appears
 function startQuiz(){
     questionContainer.style.display = 'block';
-    
+    mainContainer.style.display = 'none';
+    containerBtn.style.display = 'none';
+    questionIndex = 0;
+
+    setTime();
+    setQuestion();
+    checkAnswer();
 }
 
+function setQuestion(id) {
+    if (id < questions.length) {
+        questionEl.textContent = questions[id].Question;
+        choice1.textContent = questions[id].choices[0];
+        choice2.textContent = questions[id].choices[1];
+        choice3.textContent = questions[id].choices[2];
+        choice4.textContent = questions[id].choices[3];
 
+    }
+}
+
+function checkAnswer(event) {
+    event.preventDefault();
+
+    if (questions[questionIndex].correctAnswer === event.target.value) {
+    
+    } else if (questions[questionIndex].correctAnswer !== event.target.value) {
+        secondsLeft = secondsLeft - 10;
+    }
+    
+}
 
 
 
@@ -63,14 +88,15 @@ function startQuiz(){
 var timer = document.getElementById('timer');
 var secondsLeft = 75;
 
-// Function starts timer
+// Function to start timer
 function setTime() {
     var timerInterval = setInterval(function() {
     secondsLeft--;
     timer.textContent = secondsLeft;
 
-    if (secondsLeft === 0) {
-        clearInterval(timerInterval);    
+    if (secondsLeft === 0 || questionIndex === questions.length) {
+        clearInterval(timerInterval);   
+        questionContainer.style.display = 'none' 
     }
 }, 1000)
 }
@@ -78,10 +104,9 @@ function setTime() {
 
 // When button is clicked, timer starts and first questions appears
 startbtn.addEventListener('click', function () {
-    mainContainer.style.display = 'none';
-    containerBtn.style.display = 'none';
-    startQuiz()
-    setTime()
+            startQuiz()
+    
+
 
     }
 
